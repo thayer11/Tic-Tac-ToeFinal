6bd5f8bf7972a37e6375acd12cfcb52f116671a0
thayer11 <holly.coddington@gmail.com>
2016-06-26 19:09:24 -0600

    css-images

--- tic.js
+++ tic.js
@@ -1,23 +1,36 @@
-// function Box(id) {
-// 	this.content = " ";
-// 	this.color = " ";
-// 	this.location = id;
-// }
-// var Box1 = new Box("a");
-// var Box2 = new Box("b");
-// var Box3 = new Box("c");
-// var Box4 = new Box("d");
-// var Box5 = new Box("e");
-// var Box6 = new Box("f");
-// var Box7 = new Box("g");
-// var Box8 = new Box("h");
-// var Box9 = new Box("i");
+ function box(id) {
+	this.content = " ";
+ 	this.color = " ";
+	this.location = id;
+ }
+ var box1 = new box("a");
+ var box2 = new box("b");
+ var box3 = new box("c");
+ var box4 = new box("d");
+ var box5 = new box("e");
+ var box6 = new box("f");
+ var box7 = new box("g");
+ var box8 = new box("h");
+ var box9 = new box("i");
 
-function Turn(){
-	this.display = "X";
-	this.boxColor = "Blue";
+function turn(){
+	this.content = "X";
+	this.color = "Blue";
 }
-Turn.prototype = {
+turn.prototype = {
+	turn: function(){
+		var message = document.getElementbyId("message");
+		if ("Blue" === this.color){
+			this.color = "Green";
+			this.content = "O";
+		}
+		else {
+			this.color = "Blue";
+			this.content = "X";
+		}
+		message.textContent = this.display + "Goes!";
+		}
+	},
 	nextMove: function(box) {
 			if (this.box.textContent = " "){
 			this.box.textContent = this.content;
@@ -27,23 +40,11 @@ Turn.prototype = {
 			alert("Please choose another square, this square is already taken!");
 		}
 	},
-	switch: function(){
-		var message = document.getElementbyId("message");
-		if ("Blue" === this.color){
-			this.boxColor = "Green";
-			this.display = "O";
-		}
-		else {
-			this.boxColor = "Blue";
-			this.display = "X";
-		}
-		message.textContent = this.display + "Goes!";
-	},
 	reset: function(){
 		var gameBoxes = document.getElementsByClassName("box");
 		for (var i = 0; i < gameBoxes.length; i++){
-			gameBoxes[i].style.backgroundColor='beige';
-			gameBoxes[i].textContent='';
+			this.gameBoxes[i].style.backgroundColor='beige';
+			this.gameBoxes[i].textContent='';
 		} 
 	}
 }
