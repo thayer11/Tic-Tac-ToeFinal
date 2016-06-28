ad5dd98c8a2f8d82ddf9a94721ee03009d0ec9d3
thayer11 <holly.coddington@gmail.com>
2016-06-27 19:39:50 -0600

    partialSquareFiring

--- tic.js
+++ tic.js
@@ -1,4 +1,5 @@
- function box(id) {
+window.onload=function() {
+function box(id) {
 	this.content = " ";
  	this.color = " ";
 	this.location = id;
@@ -13,13 +14,15 @@
  var box8 = new box("h");
  var box9 = new box("i");
 
-function turn(){
-	this.content = "X";
-	this.color = "Blue";
+
+function Turn(content, color){
+	this.content= content;
+	this.color = color;
 }
-turn.prototype = {
+
+Turn.prototype = {
 	turn: function(){
-		var message = document.getElementbyId("message");
+		var message = document.getElementById("message");
 		if ("Blue" === this.color){
 			this.color = "Green";
 			this.content = "O";
@@ -29,13 +32,16 @@ turn.prototype = {
 			this.content = "X";
 		}
 		message.textContent = this.display + "Goes!";
-		}
-	},
+		},
 	nextMove: function(box) {
-			if (this.box.textContent = " "){
-			this.box.textContent = this.content;
-			this.box.style.backgroundColor = this.color;
-			// switch: function();
+		console.log('in here');
+			console.log(box);
+			if (box.content = " "){
+			box.textContent = this.content;
+			box.color = this.color;
+			this.turn();
+			for (var i = 0; i < 9; i++);
+			document.querySelector("#a").innerHTML = this.content; 		
 		} else {
 			alert("Please choose another square, this square is already taken!");
 		}
@@ -47,6 +53,20 @@ turn.prototype = {
 			this.gameBoxes[i].textContent='';
 		} 
 	}
+}
+
+var move = new Turn("X","Blue");
+
+
+document.querySelector("#a").addEventListener("click",function(){move.nextMove(box1)});
+document.querySelector("#b").addEventListener("click",function(){move.nextMove(box2)});
+document.querySelector("#c").addEventListener("click",function(){move.nextMove(box3)});
+document.querySelector("#d").addEventListener("click",function(){move.nextMove(box4)});
+document.querySelector("#e").addEventListener("click",function(){move.nextMove(box5)});
+document.querySelector("#f").addEventListener("click",function(){move.nextMove(box6)});
+document.querySelector("#g").addEventListener("click",function(){move.nextMove(box7)});
+document.querySelector("#h").addEventListener("click",function(){move.nextMove(box8)});
+document.querySelector("#i").addEventListener("click",function(){move.nextMove(box9)});
 }
 
 
